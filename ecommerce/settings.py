634fbefb477080c2a278dc29e6379a857491e9ec
Shaolong Li <shaolongli26@gmail.com>
2024-04-19 00:02:05 -0700

    add redis

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,6 +124,18 @@ DATABASES = {
     }
 }
 
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
