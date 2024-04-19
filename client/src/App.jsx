b4b2b7e1a8e2632bc8226ed7f957477d2bb1240b
shaolongli <shaolongli26@gmail.com>
2024-04-18 19:55:20 -0700

    add item overview page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home"
+import ItemPage from "./pages/ItemPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="item/:id" element={<ItemPage/>}/>
       </Routes>
     </BrowserRouter>
   );
